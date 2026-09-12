cd9d12f1779d59dcec9aa0adc1aa11b61b7a788e
Meeran-O-Meter <meeran2resource@gmail.com>
2026-09-12 21:28:32 +0400

    Visual of toggle button, no animation

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ toggleButton.addEventListener("click", () => {
     toggle = !toggle;
 
     if (toggle) {
-        toggleButton.textContent = "Toggle is On";
         document.getElementById("normalContainer").style.display = "none";
         document.getElementById("pixlContainer").style.display = "block";
     } else {
-        toggleButton.textContent = "Toggle is Off";
         document.getElementById("normalContainer").style.display = "block";
         document.getElementById("pixlContainer").style.display = "none";
     };
