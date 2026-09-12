c682445fb70ef55fd5355e1b5eeed983aa15e27a
Meeran-O-Meter <meeran2resource@gmail.com>
2026-09-12 22:26:51 +0400

    Toggle, new Stylesheet for pixl mode, Main design of pixl Mode

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+const toggleImg = document.getElementById("toggleImg");
 const toggleButton = document.getElementById("toggleButton");
 let toggle = false;
 
-toggleButton.addEventListener("click", () => {
+toggleImg.addEventListener("click", () => {
     toggle = !toggle;
 
     if (toggle) {
         document.getElementById("normalContainer").style.display = "none";
         document.getElementById("pixlContainer").style.display = "block";
+        document.querySelector("link[rel='stylesheet']").setAttribute('href', './pixl-style.css')
     } else {
         document.getElementById("normalContainer").style.display = "block";
         document.getElementById("pixlContainer").style.display = "none";
+        toggleImg.classList.remove("active")
+        toggleButton.classList.remove("active")
+        document.querySelector("link[rel='stylesheet']").setAttribute('href', './style.css')
     };
 
 
